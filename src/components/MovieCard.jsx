0572ebf37907d5742c61f7faec90cb53bbbac1ae
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-03 21:43:49 -0500

    Features: add styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,16 @@
-export function MovieCard(){
-  return  <h1>Movie Card</h1>
+import "../components/MovieCard.css"
+
+export function MovieCard({ movie }) {
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <li className="movieCard">
+      <img className="movieImage"
+      width={230}
+      height={345}
+      src={imageUrl}
+      alt={movie.title}
+      />
+      <div>{movie.title}</div>
+    </li>
+  );
 }
