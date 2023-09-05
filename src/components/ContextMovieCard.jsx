fb1eca0185a57271a834e36c506aa3a812e3d44b
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-05 13:19:53 -0500

    feature: end step 4

--- src/components/ContextMovieCard.jsx
+++ src/components/ContextMovieCard.jsx
@@ -15,7 +15,7 @@ export function ContextMovieCard() {
   return (
     <ul className="container">
       {movies.map((movie)=>(
-        <MovieCard movie={movie}/>
+        <MovieCard key={movie.id} movie={movie}/>
       ))}
     </ul>
   );
